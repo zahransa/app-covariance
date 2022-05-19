c363143416bf0993248780490896308728a587c9
SaeedZahran <saeedzahran@hotmail.com>
2022-05-19 15:46:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ with open('config.json') as config_json:
 
 # Read the raw and cov file
 data_file_raw = config.pop('fif')
-data_file_cov = config.pop('cov')
+#data_file_cov = config.pop('cov')
+
+
 
 # crop() the Raw data to save memory:
 raw = mne.io.read_raw_fif(data_file_raw, verbose=False).crop(tmax=60)
 
-cov = mne.read_cov(data_file_cov)
+noise_cov = mne.compute_raw_covariance(raw, tmin=0, tmax=None)
+
+#cov = mne.read_cov(data_file_cov)
 report = mne.Report(title='Covariance example')
-report.add_covariance(cov=data_file_cov, info=data_file_raw, title='Covariance')
+#report.add_covariance(cov=data_file_cov, info=data_file_raw, title='Covariance')
+report.add_covariance(noise_cov, info=data_file_raw, title='Covariance')
 
 
 report.save('out_dir_report/report_cov.html', overwrite=True)
